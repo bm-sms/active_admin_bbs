124a4e33ed29369a7a474d5e25bb56ff3dd097eb
kajisha <kajisha@gmail.com>
2016-09-30 15:20:02 +0900

    tweak index/show

--- app/admin/bbs_avatar.rb
+++ app/admin/bbs_avatar.rb
@@ -1,10 +1,41 @@
 ActiveAdmin.register Bbs::Avatar do
+  index do
+    column 'ID', :id
+    column 'avatar', :avatar do |o|
+      image_tag(o.avatar.url(:medium))
+    end
+
+    actions
+  end
+
+  show do |r|
+    attributes_table do
+      row :avatar do
+        image_tag(r.avatar.url(:medium))
+      end
+
+      row :url do
+        r.avatar.url
+      end
+
+      row :url_medium do
+        r.avatar.url(:medium)
+      end
+
+      row :url_thumb do
+        r.avatar.url(:thumb)
+      end
+
+      active_admin_comments
+    end
+  end
+
   form multipart: true, as: 'bbs_avatar' do |f|
     f.semantic_errors
     f.inputs Bbs::Avatar do
       f.input :avatar, as: :file
+      f.actions
     end
-    f.actions
   end
 
   controller do
