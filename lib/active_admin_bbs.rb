ad760cb1cf98bbd09f85cd8b61c2cd9509b7ebce
kajisha <kajisha@gmail.com>
2016-10-07 12:10:14 +0900

    requiure files that draw charts

--- lib/active_admin_bbs.rb
+++ lib/active_admin_bbs.rb
@@ -1,4 +1,8 @@
 require 'active_admin_bbs/version'
+require 'active_admin_bbs/translate'
+require 'active_admin_bbs/dashboard'
+require 'chartkick'
+require 'groupdate'
 require 'rails/active_admin_bbs/railtie'
 
 module ActiveAdminBbs
