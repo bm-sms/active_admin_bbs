8a4d4af121c270a9226ac25ef2bf9e1902589242
kajisha <kajisha@gmail.com>
2016-10-28 17:35:49 +0900

    bbs_user: disable new resource creation

--- app/admin/bbs_user.rb
+++ app/admin/bbs_user.rb
@@ -1,24 +1,3 @@
 ActiveAdmin.register Bbs::User do
-  form as: 'bbs_user' do |f|
-    f.semantic_errors
-
-    f.input :email
-
-    f.inputs do
-      f.has_many :profile, new_record: false do |p|
-        p.input :avatar_url
-        p.input :nickname
-      end
-    end
-
-    f.actions
-  end
-
-  controller do
-    private
-
-    def permitted_params
-      params.permit bbs_user: [:email, profile_attributes: [:avatar_url, :nickname]]
-    end
-  end
+  actions :all, except: %i(new)
 end
